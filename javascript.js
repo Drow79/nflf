a2714a13ac83f2a50c74f011118e793e171296df
Aidan <aidans80@outlook.com>
2018-02-07 02:23:30 +1100

    Refactored javascript, fixed vertical align delay issue

--- javascript.js
+++ javascript.js
@@ -3,17 +3,20 @@
 
 $(document).ready(function() {
    
-   elementTop = ($(".vertical-align-middle").parent().height() / 2) - ($(".vertical-align-middle").height() / 2);
-   $(".vertical-align-middle").css('margin-top', elementTop);
-
+   updateVerticalAlignment();
+   
    /* Window Resize Event */
    $(window).resize(function() {
-      //$(".vertical-align-middle").css('transition', '0.3s margin');
-      setTimeout(function() {
-         elementTop = ($(".vertical-align-middle").parent().height() / 2) - ($(".vertical-align-middle").height() / 2);
-         $(".vertical-align-middle").css('margin-top', elementTop);
-         console.log("Vertically aligning element by", ($(".vertical-align-middle").parent().height() / 2) - ($(".vertical-align-middle").height() / 2));
-      }, 300);
+      updateVerticalAlignment();
    });
    
 });
+
+function updateVerticalAlignment() {
+   elementTop = ($(".vertical-align-middle").parent().height() / 2) - ($(".vertical-align-middle").height() / 2);
+   $(".vertical-align-middle").css('margin-top', elementTop);
+   setTimeout(function() {
+      elementTop = ($(".vertical-align-middle").parent().height() / 2) - ($(".vertical-align-middle").height() / 2);
+      $(".vertical-align-middle").css('margin-top', elementTop);
+   }, 300);
+}
